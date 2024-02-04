e53c5f0e497ea825875190caf467f5b2b70363e1
Juan Reyes <camiloreyes1125@gmail.com>
2024-02-03 23:16:04 -0500

    nba live commit

--- src/pages/Nba.tsx
+++ src/pages/Nba.tsx
@@ -1,7 +1,41 @@
-const Nba = () => {
-  return (
-    <div>Nba</div>
-  )
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface NbaProps {
+  name: string;
 }
 
-export default Nba
+const Nba: React.FC<NbaProps> = ({ name }) => {
+  const [response, setResponse] = useState(null);
+
+  const fetchNbaLive = async () => {
+    try {
+      const res = await axios.get('https://api-nba-v1.p.rapidapi.com/games', {
+        headers: {
+          'X-RapidAPI-Key': process.env.API_NBA_KEY,
+          'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
+        },
+        params: { q: `${name}` },
+      });
+
+      setResponse(res.data);
+      console.log(res.data);
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    console.log("Teams ==>", name)
+    fetchNbaLive()
+  }, [name]);
+
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
+export default Nba;
